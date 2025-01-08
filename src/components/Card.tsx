bff57b69186583fe03b12f61be6fa4823ff5bcc1
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-01-08 09:28:03 +0530

    Adding Document Manager (FC)

---
+++ src/components/Card.tsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { MdOutlineAttachFile } from "react-icons/md";
+import { MdOutlineDownloadForOffline } from "react-icons/md";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { motion } from "framer-motion";
+
+function Card({ data, reference }) {
+  return (
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.1 }}
+      dragElastic={0.2}
+      whileTap={{ scale: 0.8 }}
+    >
+      <div className="relative w-60 h-[300px] rounded-[40px] px-8 py-10 bg-zinc-900/50 text-gray-200 p-5 overflow-hidden">
+        <MdOutlineAttachFile />
+        <p className="text-sm leading-tight mt-5">{data.desc}</p>
+        <div className="w-full h-10 footer flex items-center mt-20 justify-between ">
+          <p className="text-sm">{data.size}</p>
+          {data.close ? (
+            <IoMdCloseCircleOutline size={20} color="grey" />
+          ) : (
+            <MdOutlineDownloadForOffline size={20} color="grey" />
+          )}
+        </div>
+        {data.tag.isOpen && (
+          <div
+            className={`w-full h-12 py-3 absolute ${
+              data.tag.tagColor === "blue" ? "bg-green-500" : "bg-blue-500"
+            } bottom-0 left-0 text-white`}
+          >
+            <button
+              type="button"
+              className="w-full text-center text-[12px] cursor-pointer"
+            >
+              {data.tag.tagTitle}
+            </button>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  );
+}
+
+export default Card;
---
+++ src/components/Foreground.tsx
@@ -0,0 +1,76 @@
+import React, { useRef } from "react";
+import Card from "./Card";
+
+function Foreground() {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti esse tenetur voluptatem accusamus cumque! Obcaecati aliquam vel totam dolore explicabo!",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet lorem20 elit.",
+      size: ".4mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+  ];
+
+  return (
+    <div
+      ref={ref}
+      className="fixed z-[3] top-0 left-0 w-full h-full flex py-10 px-2 gap-10  flex-wrap"
+    >
+      {data.map((item, index) => (
+        <Card data={item} reference={ref} />
+      ))}
+    </div>
+  );
+}
+
+export default Foreground;
